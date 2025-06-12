1a0501b4740a39be69bfa11e462ecc90eb24cad4
Gustavo Resende <gustavo@resende.app>
2025-06-12 11:41:44 -0300

    import oxygen font

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import './i18n';
+import '@fontsource/oxygen/400.css';
+import '@fontsource/oxygen/700.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
